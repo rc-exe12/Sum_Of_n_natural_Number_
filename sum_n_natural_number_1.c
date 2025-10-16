7d5cb1c321d4c3a4fe72061bf93c86c85d5aba9c
Ramcharan Sai <25r11a05ff@gcet.edu.in>
2025-10-16 20:09:35 +0530

    Add files via upload

---
+++ sum_n_natural_number_1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int i=1,n,sum=0;
+    printf("Enter a n value");
+    scanf("%d",&n);
+while(i<=n)
+{
+    sum=sum+i;
+i++;
+}
+printf("The sum of %d numbers is: %d",n,sum);
+return 0;
+
+}
