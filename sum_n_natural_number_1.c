47b85fa700452626a1e8c8e0cf5a4afd1b2332ee
Ramcharan Sai <25r11a05ff@gcet.edu.in>
2025-11-28 13:47:41 +0530

    With Functions

--- sum_n_natural_number_1.c
+++ sum_n_natural_number_1.c
@@ -1,14 +1,18 @@
 #include<stdio.h>
+void n_sum(int);
 int main(){
-    int i=1,n,sum=0;
+    int n;
     printf("Enter a n value");
     scanf("%d",&n);
-while(i<=n)
+    n_sum(n);
+return 0;
+}
+void n_sum(int n){
+int i=1,sum=0;
+    while(i<=n)
 {
     sum=sum+i;
 i++;
 }
-printf("The sum of %d numbers is: %d",n,sum);
-return 0;
-
+printf("The sum of  numbers is: %d",sum);
 }
