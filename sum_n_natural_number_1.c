b38e442d2306951b30c1df9a2751bf2113b9b909
Ramcharan Sai <25r11a05ff@gcet.edu.in>
2025-11-29 10:45:53 +0530

    Refactor sum calculation to use recursion

--- sum_n_natural_number_1.c
+++ sum_n_natural_number_1.c
@@ -1,18 +1,21 @@
 #include<stdio.h>
-void n_sum(int);
+int n_sum(int);
 int main(){
     int n;
     printf("Enter a n value");
     scanf("%d",&n);
-    n_sum(n);
+    int r=n_sum(n);
+    printf("The sum is %d",r);
 return 0;
 }
-void n_sum(int n){
-int i=1,sum=0;
-    while(i<=n)
-{
-    sum=sum+i;
-i++;
+int n_sum(int n){
+    int sum=0;
+if(n==1){
+    return 1;
 }
-printf("The sum of  numbers is: %d",sum);
+else{
+   return  n+n_sum(n-1); 
+    
 }
+}
+
